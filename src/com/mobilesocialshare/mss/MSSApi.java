89855194bdd75de2b9ead9240a0760af81c14c66
Victor Pantoja <victor.pantoja@gmail.com>
2012-03-10 16:57:13 -0300

    changing sendContext method to post data

--- src/com/mobilesocialshare/mss/MSSApi.java
+++ src/com/mobilesocialshare/mss/MSSApi.java
@@ -7,6 +7,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.http.HttpEntity;
@@ -14,10 +18,13 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -32,7 +39,7 @@ public class MSSApi {
 	private static final String TAG = "mss-api";
 	
 	//public static final String server_name = "http://myalbumshare.com:8000/api";
-	private String server_name = "http://192.168.0.255:9080";
+	private String server_name = "http://192.168.0.191:9080";
 	private static final String url_api_information = "/status";
 	private static final String url_login = "/login";
 	private static final String url_create_acount = "/login/create";
@@ -149,16 +156,44 @@ public class MSSApi {
 		return "";
 	}
 
-	public String CreateUser(String lastName, String firstName, String username, String gender) {
-		String url = url_create_acount+"?username="+username+"&firstName="+firstName+"&lastName="+lastName+"&gender="+gender;
+	public String CreateUser(String email, String lastName, String firstName, String username, String gender) {
+		String url = url_create_acount+"?username="+username+"&firstName="+firstName+"&lastName="+lastName+"&email="+email+"&gender="+gender;
 		
 		return queryRESTurl(url);
 	}
 
-	public String SendContext(Map<Integer, String> context, String auth) {
-		String url = url_send_context+"?location="+"teste"+"&text="+"qqcosa"+"&auth="+auth;
+	public String sendContext(Map<String, String> context, List<String> apps, String auth) {
+	    HttpClient httpclient = new DefaultHttpClient();
+	    HttpPost httppost = new HttpPost(this.server_name+url_send_context+"?auth="+auth);
+	    	    
+	    List<String> apps_str = new ArrayList<String>();
+	    
+	    for(String app : apps){
+	    	apps_str.add("\""+app+"\"");
+	    }
+	    	    
+	    try {
+		    StringEntity se = new StringEntity("{\"application\":"+apps_str.toString()+",\"context\":{\"location\":\""+context.get("location")+"\",\"status\":\""+context.get("status")+"\"}}",HTTP.UTF_8);
+	    	
+		    httppost.setHeader("Content-Type","application/json;charset=UTF-8");
+	    	httppost.setEntity(se);
 
-		return queryRESTurl(url);
+	        HttpResponse response = httpclient.execute(httppost);
+	        HttpEntity entity = response.getEntity();
+	        
+			if (entity != null) {  
+				InputStream instream = entity.getContent();  
+				String result = convertStreamToString(instream);  
+				instream.close();  
+				return result;  
+			}  
+	        
+	    } catch (ClientProtocolException e) {
+	    	 Log.e(TAG,e.getMessage());
+	    } catch (IOException e) {
+	        Log.e(TAG,e.getMessage());
+	    }
+	    return "";
 	}
 	
 	public String GetUserInformation(String username, String auth){
@@ -185,7 +220,7 @@ public class MSSApi {
 		return queryRESTurl(url);
 	}
 	
-	public String GetFrind(String auth){
+	public String GetFriends(String auth){
 		String url = url_get_friend+"?auth="+auth;
 		return queryRESTurl(url);
 	}
@@ -199,6 +234,25 @@ public class MSSApi {
 		String url = url_send_email_envite+"?email="+email+"&auth="+auth;
 		
 		return queryRESTurl(url);
+	}
+	
+	public static String md5(String s) {
+	    try {
+	        // Create MD5 Hash
+	        MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+	        digest.update(s.getBytes());
+	        byte messageDigest[] = digest.digest();
+	        
+	        // Create Hex String
+	        StringBuffer hexString = new StringBuffer();
+	        for (int i=0; i<messageDigest.length; i++)
+	            hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+	        return hexString.toString();
+	        
+	    } catch (NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	    }
+	    return "";
 	}
 	
 }
